fc15c33afde9706c6248849ecc0c96f2d8f381ea
sukexin <1256414520@qq.com>
2020-11-14 17:01:33 +0800

    撒打算大所多所

--- 1.java
+++ 1.java
@@ -1,13 +1,22 @@
-class Test{
+class Test {
     public static void main(String[] args) {
         System.out.println("Hello Word");
-         System.out.println("Test agin");
-         System.out.println("Test agin");
-         System.out.println("Test agin");
-         System.out.println("Test agin");
-         System.out.println("Test agin");
-         System.out.println("Test agin");
-         System.out.println("Test agin");
-        
+        System.out.println("Test agin");
+        System.out.println("Test agin");
+        System.out.println("Test agin");
+        System.out.println("Test agin");
+        System.out.println("Test agin");
+        System.out.println("Test agin");
+        System.out.println("Test agin");
+//        **********************本地idea进行上传改变*****************************************
+        System.out.println("idea.....................................................");
+        //        **********************本地idea进行上传改变*****************************************
+        System.out.println("idea.....................................................");
+        //        **********************本地idea进行上传改变*****************************************
+        System.out.println("idea.....................................................");
+        //        **********************本地idea进行上传改变*****************************************
+        System.out.println("idea.....................................................");
+        //        **********************本地idea进行上传改变*****************************************
+
     }
 }
