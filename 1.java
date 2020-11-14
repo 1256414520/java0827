c7efd831073c13a3dcd4fdc98ab171085ef15c05
sukexin <1256414520@qq.com>
2020-11-14 17:10:06 +0800

    再次线下修改代码

--- 1.java
+++ 1.java
@@ -8,7 +8,7 @@ class Test {
         System.out.println("Test agin");
         System.out.println("Test agin");
         System.out.println("Test agin");
-      **********************本地idea进行上传改变  被驳回了   需要你自己在跟新到本地仓库*****************************************
+      //**********************本地idea进行上传改变  被驳回了   需要你自己在跟新到本地仓库*****************************************
   System.out.println("rollback");
 
     }
